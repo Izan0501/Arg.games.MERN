d204ada58e1251f3e8b6d35f42110ee6c3e4c655
Izan <ivozanacchi501@gmail.com>
2023-09-25 17:22:37 -0300

    addLinkHeaderProps

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavHeader from '../subComponents/NavHeader'
 import '../css/header.css'
 
 export const Header = () => {
@@ -17,22 +18,16 @@ export const Header = () => {
                     viewBox="0 0 16 16"
                 >
                     <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
                     />
                 </svg>
             </div>
             <ul className="navegation">
-                <li>
-                    <a href="./pages/login.html">Login</a>
-                </li>
-                <li>
-                    <a href="#">Play Online</a>
-                </li>
-                <li>
-                    <a href="#">Games</a>
-                </li>
-                <li>
+                <NavHeader content={'Login'} />
+                <NavHeader content={'Play Online'} />
+                <NavHeader content={'Games'} />
+                <li> 
                     <a href="#">
                         <div className="btn-group">
                             <button
