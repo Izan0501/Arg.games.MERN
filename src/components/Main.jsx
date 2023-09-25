295d111c913d4c623cbda0404ee4d72f24bb5c1a
Izan <ivozanacchi501@gmail.com>
2023-09-25 18:40:18 -0300

    addMainWthStyless

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/main.css'
+
+export const Main = () => {
+    return (
+        <main className="contentTxt">
+            <div className="txtDesctipt">
+                <h2>What's New</h2>
+                <p>
+                    On this page you can find the highest-grossing games in the gaming world that currently covers available with new graphic and playability capabilities offered by new generation consoles such as Xbox series, PS5, switch, PC, among others. Sections for Xbox, PC and Switch will soon be available.
+                </p>
+                <a href="#">More Info</a>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
